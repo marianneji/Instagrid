4a3addf3532d8c046b7c70c1819796570bd9d598
marianneji <marianneji@gmail.com>
2019-04-29 13:51:01 +0200

    Initial Commit

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Graphic Influence on 16/04/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagrid/Model/Layout.swift
@@ -0,0 +1,13 @@
+//
+//  Layout.swift
+//  Instagrid
+//
+//  Created by Graphic Influence on 25/04/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import Foundation
+//
+enum Layout {
+    case layout1, layout2, layout3
+}

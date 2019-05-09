c54fa22c0fe1e3cb38c0995aa56cb6de3b635f36
marianneji <marianneji@gmail.com>
2019-05-09 16:04:15 +0200

    add outlet to photosLayoutView class and method to setup layouts

--- Instagrid/Model/Layout.swift
+++ Instagrid/Model/Layout.swift
@@ -8,30 +8,37 @@
 
 import Foundation
 
-
+import UIKit
 
 enum Layout: Int {
-    case layout1 = 1
-    case layout2
-    case layout3
+    case layout1 = 1, layout2, layout3
     
-    var photosLayoutNumber: PhotosLayoutNumber {
+    var photosLayoutNumber: (top: Int, bot: Int) {
         switch self {
         case .layout1:
-            return PhotosLayoutNumber(top: 1, bot: 2)
+            return (top: 1, bot: 2)
         case .layout2:
-            return PhotosLayoutNumber(top: 2, bot: 1)
+            return (top: 2, bot: 1)
         case .layout3:
-            return PhotosLayoutNumber(top: 2, bot: 2)
+            return (top: 2, bot: 2)
         }
     }
-    
 }
 
-struct PhotosLayoutNumber {
+class PhotosLayoutView: UIView {
     
-    let top: Int
+    @IBOutlet weak var topLeftView: UIView?
+    @IBOutlet weak var topRightView: UIView?
+    @IBOutlet weak var bottomLeftView: UIView?
+    @IBOutlet weak var bottomRightView: UIView?
     
-    let bot: Int
+    func setupLayout(_ layout: Layout) {
+        self.topLeftView?.isHidden = false
+        self.topRightView?.isHidden = layout.photosLayoutNumber.top == 1
+        self.bottomLeftView?.isHidden = false
+        self.bottomRightView?.isHidden = layout.photosLayoutNumber.bot == 1
+
+    }
+    
 }
 

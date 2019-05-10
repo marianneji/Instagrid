14153f3bb17435425515de2d6f06e2180b47cd51
marianneji <marianneji@gmail.com>
2019-05-10 14:53:40 +0200

    Add new File cocoa to View with PhotosLayoutView Connect IBOutlets

---
+++ Instagrid/View/PhotosLayoutView.swift
@@ -0,0 +1,25 @@
+//
+//  PhotosLayoutView.swift
+//  Instagrid
+//
+//  Created by Graphic Influence on 10/05/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import UIKit
+
+class PhotosLayoutView: UIView {
+    
+    @IBOutlet weak var topLeftView: UIView?
+    @IBOutlet weak var topRightView: UIView?
+    @IBOutlet weak var bottomLeftView: UIView?
+    @IBOutlet weak var bottomRightView: UIView?
+    
+    func setupLayout(_ layout: Layout) {
+        self.topLeftView?.isHidden = false
+        self.topRightView?.isHidden = layout.photosLayoutNumber.top == 1
+        self.bottomLeftView?.isHidden = false
+        self.bottomRightView?.isHidden = layout.photosLayoutNumber.bot == 1
+        
+    }
+}
